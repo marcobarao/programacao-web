452f97fdb9be14fa17c12c0ffdf33190ea23c543
MARCO ANTONIO BARAO NEVES <marco.abneves@senacsp.edu.br>
2019-04-29 21:21:16 -0300

    Finalizando exercicios js repeticao

--- js-02/exercicios/15/index.js
+++ js-02/exercicios/15/index.js
@@ -1,4 +1,8 @@
 var res = "";
 do {
+  if (res) {
+    alert("Apenas aceitarei palavra com 6 caracteres");
+  }
+
   res = prompt("Digite uma palavra iniciando com P ou que contenha F");
 } while(/$p+/gi.test(res) || /f/gi.test(res));
